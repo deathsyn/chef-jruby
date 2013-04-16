a90e185d7365639fcf5e3e12b9916bb5c69f5085
Kevin Berry <kevin@opensourcealchemist.com>
2013-04-16 10:54:02 -0500

    Adjust default version attribute and checksum. Closes #1.
    Notify the jruby.sh profile install immediately to
    let it be available to install gems immediately afterward.
    Closes #2.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
-default[:jruby][:version] = "1.6.7.2"
-default[:jruby][:checksum] = "1e520f1b5130114464e5f1950cb24774"
+default[:jruby][:version] = "1.7.3"
+default[:jruby][:checksum] = "f0adf8ccee6f6777f7ab8e5e1d7a1f47"
 default[:jruby][:install_path] = "/usr/local/lib/jruby"
 default[:jruby][:gems] = []
 default[:jruby][:nailgun] = false
